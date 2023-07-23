9b2596bf1a0cc34368a10b4c575c29aba3b4cf38
Jedi Maker <jedi@wearemaker.studio>
2023-07-23 14:04:40 -0500

    type inferred from IconsMap record

--- packages/nextjs/components/Icons/index.tsx
+++ packages/nextjs/components/Icons/index.tsx
@@ -2,32 +2,13 @@ import React from "react";
 import LogoutIcon from "./LogoutIcon";
 import { MainIconProps } from "./icons.type";
 
-interface IconProps extends MainIconProps {
-  title:
-    | "logout"
-    | "copy"
-    | "users"
-    | "star"
-    | "approximate"
-    | "beneficiaries"
-    | "important"
-    | "user"
-    | "wallet-blue"
-    | "magic-wand"
-    | "beneficiaries-blue"
-    | "line"
-    | "stop-watch"
-    | "import"
-    | "chevron-left"
-    | "copy-purple"
-    | "edit"
-    | "delete"
-    | "delete-orange";
-}
-
 const IconsMap: Record<string, React.FC<MainIconProps>> = {
   logout: LogoutIcon,
 };
+
+interface IconProps extends MainIconProps {
+  title: keyof typeof IconsMap;
+}
 
 const Icon = ({ title, ...rest }: IconProps) => {
   const IconComponent = IconsMap[title];
