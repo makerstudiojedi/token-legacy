0ecddaf8f556b65f4e329f71b96de89c0e813527
Jedi Maker <jedi@wearemaker.studio>
2023-07-23 16:48:47 -0500

    dev setup for managing faucet, balance and accounts

---
+++ packages/nextjs/components/DevAccount/index.tsx
@@ -0,0 +1,16 @@
+import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
+
+const DevAccount = () => {
+  return (
+    <div className="absolute w-full bottom-4">
+      <div className="flex flex-1 justify-center">
+        <div className="flex max-w-md p-2 bg-blue-950 items-center justify-center rounded">
+          <RainbowKitCustomConnectButton />
+          <FaucetButton />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DevAccount;
